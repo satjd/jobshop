ab972eafbfe1c76a1c1b6e6e8a00380fa4b1a4cc
Songao-Bit <824909419@qq.com>
2016-06-01 14:49:46 +0800

    bug fixed

--- src/jobshop/algorithm/metaheuristic/PSOalgorithm.java
+++ src/jobshop/algorithm/metaheuristic/PSOalgorithm.java
@@ -1,6 +1,7 @@
 package jobshop.algorithm.metaheuristic;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 import jobshop.calctime.Scheduler;
 
@@ -10,7 +11,22 @@ public class PSOalgorithm
 	public static void main(String[] args) throws FileNotFoundException
 	{
 		// TODO 自动生成的方法存根
-		System.out.println(1.0/Scheduler.getParticleFromMachineSet().getFitness());
+		
+		int iteration = 1000; //迭代次数
+		int swarmSize = 100; //微粒个数
+		
+		ArrayList<Particle> swarm = new ArrayList<Particle>();
+		for(int i=1;i<=swarmSize;i++)
+			swarm.add(Scheduler.getParticleFromMachineSet()); //随机生成初代微粒，位置随机
+		
+		for(int i=1;i<=iteration;i++) //开始迭代
+		{
+			
+			for(Particle p:swarm)
+			{
+				
+			}
+		}
 	}
 
 }
--- src/jobshop/algorithm/metaheuristic/Particle.java
+++ src/jobshop/algorithm/metaheuristic/Particle.java
@@ -4,6 +4,9 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Random;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 import jobshop.calctime.Job;
 import jobshop.calctime.Machine;
@@ -39,21 +42,28 @@ public class Particle
 	private int arraySize; //微粒位置向量每台机器分量的大小
 	private ArrayList<Machine> machineSet;  //微粒对应的机器集合
 	private ArrayList<ArrayList<Double>> wArray; //微粒的位置（向量各个分量权值 的向量）
+	private ArrayList<ArrayList<Double>> curV; //微粒的速度
 	
 	public Particle(int arraySize,ArrayList<Machine> machineSet)
 	{
 		this.arraySize = arraySize;
 		this.machineSet = machineSet;
 		this.wArray = new ArrayList<ArrayList<Double>>();
+		this.curV = new ArrayList<ArrayList<Double>>();
 		
-		ArrayList<Double> tmpL = new ArrayList<Double>();
+		ArrayList<Double> tmpLw = new ArrayList<Double>();
+		ArrayList<Double> tmpLc = new ArrayList<Double>();
 		for(int i=0;i<arraySize;i++)
-			tmpL.add((double)i);
+		{
+			tmpLw.add((double)i);
+			tmpLc.add(0.0);
+		}
 		
 		for(Machine m:machineSet)
 		{
-			Collections.shuffle(tmpL);
-			wArray.add(tmpL);
+			Collections.shuffle(tmpLw);
+			wArray.add(tmpLw);
+			curV.add(tmpLc);
 		}
 			
 		updateMachineSet();
@@ -64,16 +74,49 @@ public class Particle
 		return machineSet;
 	}
 	
-	public void updateMachineSet()
+	public void updateMachineSet() //根据向量更新微粒对应的机器调度优先顺序
 	{
 		for(Machine m:machineSet)
 		{
 			Collections.sort(m.getInstanceOfPriority(), new JobPrioirtySetter(m));
+			//m.setMachineBuffer();
 		}
 	}
 	
 	public double getFitness() throws FileNotFoundException
 	{
 		return FitnessCalc.calcFitness(this);
+	}
+	
+	public ArrayList<ArrayList<Double>> getCurV()
+	{
+		return curV;
+	}
+	
+	public ArrayList<ArrayList<Double>> getWArray()
+	{
+		return wArray;
+	}
+	
+	public void fly(Particle pBest,Particle gBest,double c1,double c2) //微粒迭代
+	{
+		Random r = new Random();
+		for(int i=0;i<curV.size();i++)
+		{
+			ArrayList<Double> v = curV.get(i); //速度的分量
+			ArrayList<Double> curArray = wArray.get(i); //位置的分量
+			ArrayList<Double> pbesti = pBest.getWArray().get(i); //pbest对应位置分量
+			ArrayList<Double> gbesti = gBest.getWArray().get(i); //gbest对应位置分量
+			
+			for(int j=0;j<v.size();j++)
+			{
+				double tmp = v.get(j);
+				double rand = r.nextDouble();
+				tmp += c1*rand*(pbesti.get(j)-curArray.get(j)) + c2*rand*(gbesti.get(j)-curArray.get(j));
+				v.set(j, tmp); //更新速度
+				curArray.set(j, tmp+curArray.get(j)); //更新位置
+			}
+		}
+		updateMachineSet(); //更新对应机器的工序优先级
 	}
 }
